048d79d24b111d4146d11458703949af0fc33085
Omar N <54334118+omnz@users.noreply.github.com>
2023-10-10 17:18:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 # Keep track of monthly expenses and return your average expenses per year.
 import os
+import json
 import Expenses
+
+def calulateExpenses():
+    dataDir = './data'
+    filename = 'expenses.json'
+    filepath = f'{dataDir}/{filename}'
+    data = ''
+    years = []
+    dataByYear = []
+
+    with open(filepath, 'r+') as infile:
+        data = json.load(infile)
+
+    print(data)
+    # Count number of years of data
+    for d in data:
+        if d['year'] not in years:
+            years.append(d['year'])
+
+    print(f'years: {years}')
+
+
+    # TODO: Calculate total monthly expenses and per category
+    
+    # TODO: Calculate total yearly expenses and per category
+    # TODO: Calculate average monthly and yearly expenses
 
 def main():
     # Create 'data' directory if it does not exist
@@ -16,6 +42,8 @@ def main():
     new_expense = Expenses.Expenses()
     new_expense.askForExpenses()
 
+    # Print expenses
+    calulateExpenses()
 
 if __name__ == '__main__':
     main()
