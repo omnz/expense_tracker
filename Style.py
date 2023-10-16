19348cb19dd7ccf2880b83dd64ad1bee20cd1f52
Omar N <54334118+omnz@users.noreply.github.com>
2023-10-16 16:53:05 -0400

    Create Style.py
    Created class dealing with the styling of the console output.

---
+++ Style.py
@@ -0,0 +1,8 @@
+class Style():
+    """Class dealing with styling the console output"""
+    def __init__(self):
+        self.BOLD = '\033[1m'
+        self.NORMAL = '\033[0m'
+        self.RED = '\033[31m'
+        self.GREEN = '\033[32m'
+        self.BLUE = '\033[34m'
